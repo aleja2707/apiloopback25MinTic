05f05b29169893ff8905b73a41d57e3c31bdfbc4
aleja2707 <alejaocampo2707@gmail.com>
2021-11-05 06:49:35 -0500

    first commit

---
+++ src/models/ruta.model.ts
@@ -0,0 +1,30 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Ruta extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  tiempo_estimado: number;
+
+
+  constructor(data?: Partial<Ruta>) {
+    super(data);
+  }
+}
+
+export interface RutaRelations {
+  // describe navigational properties here
+}
+
+export type RutaWithRelations = Ruta & RutaRelations;
---
+++ src/models/servicio.model.ts
@@ -0,0 +1,60 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Servicio extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  hora_inicio: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  hora_fin: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  placa: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre_conductor: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  dinero: number;
+
+
+
+
+  constructor(data?: Partial<Servicio>) {
+    super(data);
+  }
+}
+
+export interface ServicioRelations {
+  // describe navigational properties here
+}
+
+export type ServicioWithRelations = Servicio & ServicioRelations;
